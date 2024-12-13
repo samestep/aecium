a79a06d4f147aec1cc9035800e0698ec1abc432b
Sam Estep <sam@samestep.com>
2024-12-13 12:52:41 -0500

    Switch to rustc lexer and rust-analyzer parser

--- crates/aecium/src/cli.rs
+++ crates/aecium/src/cli.rs
@@ -1,29 +1,33 @@
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, process::ExitCode};
 
 use clap::Parser;
-
-use crate::lex::lex;
 
 #[derive(Debug, Parser)]
 struct Cli {
     file: PathBuf,
+
+    #[clap(long)]
+    edition: ra_ap_parser::Edition,
 }
 
-pub fn cli() -> Result<(), ()> {
+fn cli_result() -> Result<(), ExitCode> {
     let args = Cli::parse();
-    let source = fs::read_to_string(&args.file).map_err(|e| {
+    let text = fs::read_to_string(&args.file).map_err(|e| {
         eprintln!("failed to read {:?}: {e}", args.file);
+        ExitCode::FAILURE
     })?;
-    let tokens = lex(&source).map_err(|e| {
-        eprintln!(
-            "failed to lex {:?}: {} at byte range {:?}",
-            args.file,
-            e.message(),
-            e.byte_range(),
-        );
-    })?;
-    for token in tokens.iter() {
-        println!("{:?}", token.kind);
+    let lexed = ra_ap_parser::LexedStr::new(args.edition, &text);
+    let input = lexed.to_input(args.edition);
+    let output = ra_ap_parser::TopEntryPoint::SourceFile.parse(&input, args.edition);
+    for step in output.iter() {
+        println!("{:?}", step);
     }
     Ok(())
+}
+
+pub fn cli() -> ExitCode {
+    match cli_result() {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(code) => code,
+    }
 }
--- crates/aecium/src/main.rs
+++ crates/aecium/src/main.rs
@@ -1,13 +1,5 @@
 mod cli;
-mod lex;
-mod token;
-mod util;
 
-use std::process::ExitCode;
-
-fn main() -> ExitCode {
-    match cli::cli() {
-        Ok(()) => ExitCode::SUCCESS,
-        Err(()) => ExitCode::FAILURE,
-    }
+fn main() -> std::process::ExitCode {
+    cli::cli()
 }
