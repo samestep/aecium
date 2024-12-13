252051321e5c6c1c382cb69d9b5452d67ae89746
Sam Estep <sam@samestep.com>
2024-12-13 16:28:11 -0500

    Print a more human-readable tree

--- crates/aecium/src/cli.rs
+++ crates/aecium/src/cli.rs
@@ -19,8 +19,35 @@ fn cli_result() -> Result<(), ExitCode> {
     let lexed = ra_ap_parser::LexedStr::new(args.edition, &text);
     let input = lexed.to_input(args.edition);
     let output = ra_ap_parser::TopEntryPoint::SourceFile.parse(&input, args.edition);
+    let mut i: usize = 0;
+    let mut d: usize = 0;
     for step in output.iter() {
-        println!("{:?}", step);
+        if let ra_ap_parser::Step::Exit = step {
+            d -= 1;
+        }
+        for _ in 0..d {
+            print!("  ");
+        }
+        match step {
+            ra_ap_parser::Step::Token {
+                kind,
+                n_input_tokens,
+            } => {
+                while lexed.kind(i).is_trivia() {
+                    i += 1;
+                }
+                let n = usize::from(n_input_tokens);
+                println!("{kind:?} {:?}", lexed.range_text(i..i + n));
+                i += n;
+            }
+            ra_ap_parser::Step::FloatSplit { .. } => todo!(),
+            ra_ap_parser::Step::Enter { kind } => {
+                println!("{kind:?} {{");
+                d += 1;
+            }
+            ra_ap_parser::Step::Exit => println!("}}"),
+            ra_ap_parser::Step::Error { msg } => println!("{msg:?}"),
+        }
     }
     Ok(())
 }
